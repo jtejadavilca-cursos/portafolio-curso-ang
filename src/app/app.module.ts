7b0a3baaeb1c84fec5bba8fe6510cf3038bd403f
Jose Tejada <jtejadavilca@gmail.com>
2017-09-04 23:48:53 -0500

    Se agrega componentes about y portafolio-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortafolioItemComponent } from './components/portafolio-item/portafolio-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    PortafolioItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/portafolio-item/portafolio-item.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portafolio-item',
+  templateUrl: './portafolio-item.component.html'
+})
+export class PortafolioItemComponent {
+
+}
